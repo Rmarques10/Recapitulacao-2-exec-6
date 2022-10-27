0148cc76db014b415b6ca7dc9bd5fb2d34c34032
Rafael Marques <rafaelmarques.rm86@gmail.com>
2022-10-27 12:51:11 -0300

    Recapitulação

---
+++ Recapitulacao 2 exec 6/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos 
+seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente se o valor não estiver em 
+nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo” */
+
+
+double num;
+
+Console.WriteLine("informe um numero:");
+num = double.Parse(Console.ReadLine());
+
+
+if (num < 0.0 || num > 100.0)
+{
+    Console.WriteLine("Fora de intervalo");
+}
+else if (num <= 25.0)
+{
+    Console.WriteLine("Intervalo [0,25]");
+}
+else if (num <= 50.0)
+{
+    Console.WriteLine("Intervalo (25,50]");
+}
+else if (num <= 75.0)
+{
+    Console.WriteLine("Intervalo (50,75]");
+}
+else
+{
+    Console.WriteLine("Intervalo (75,100]");
+}
